6417ae210c911c2780c312dc50f9c9e3cb2e970c
muhamadumar <muhamadumar11@gmail.com>
2025-08-22 15:05:19 +0700

    update login

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/lib/db";
 import bcrypt from "bcrypt";
 
-const baseUrl = process.env.NEXTAUTH_URL
 export const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
@@ -13,21 +12,34 @@ export const authOptions: NextAuthOptions = {
                 password: { label: "Password", type: "password" }
             },
             async authorize(credentials) {
+                console.log("Authorize input:", credentials);
                 if (!credentials?.email || !credentials?.password) return null;
+
                 const user = await prisma.user.findUnique({ where: { email: credentials.email } });
+                console.log("User found:", user);
+
                 if (!user) return null;
+
                 const isValid = await bcrypt.compare(credentials.password, user.password);
+                console.log("Password valid:", isValid);
+
                 if (!isValid) return null;
+
                 return { id: String(user.id), name: user.name, email: user.email };
-            },
+            }
+
         }),
     ],
     pages: { signIn: "/admin/login" },
     session: { strategy: "jwt" },
     secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
-        async redirect({ url }) {
-            return url.startsWith("/") ? `${baseUrl}${url}` : url;
-        }
+        async redirect({ url, baseUrl }) {
+            // baseUrl diambil otomatis dari NEXTAUTH_URL
+            if (url.startsWith("/")) return `${baseUrl}${url}`;
+            if (new URL(url).origin === baseUrl) return url;
+            return baseUrl;
+        },
     }
+
 };
