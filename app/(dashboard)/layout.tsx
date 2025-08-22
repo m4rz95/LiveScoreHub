828c822c0cab5f25d3378b19b222326a91c2e3ea
muhamadumar <muhamadumar11@gmail.com>
2025-08-22 13:54:58 +0700

    auth session

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,12 @@
 // app/dashboard/layout.tsx
 import { redirect } from "next/navigation"
-import { getSession } from "@/lib/auth"
+import { getSession } from "@/lib/auth-session"
 import Nav from '@/components/Nav'
 import { Toaster } from 'sonner'
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   const session = await getSession()
-  
+
   if (!session) {
     redirect("/admin/login")
   }
